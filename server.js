2e8b0947eef32e29c919da3de87a738d4754e26f
keya-moradi <keya.moradi@gmail.com>
2024-10-14 02:27:56 -0700

    debug for why user is not being passed to the EJS template

--- server.js
+++ server.js
@@ -45,11 +45,12 @@ app.use(passport.session());
 // Flash middleware
 app.use(flash());
 
-// Global variables for flash messages
+// Global variables for flash messages and user
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error'); // For Passport.js errors
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null; // Make user globally available in templates
     next();
 });
 
