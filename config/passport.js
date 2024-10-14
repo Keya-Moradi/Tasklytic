0156b8d11b3cdf26b7afc3ec6eaba5a71585762a
keya-moradi <keya.moradi@gmail.com>
2024-10-14 01:47:25 -0700

    update config/passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,9 @@
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../models/User');
 
 module.exports = function(passport) {
     passport.use(
@@ -10,9 +14,16 @@ module.exports = function(passport) {
                     if (!user) {
                         return done(null, false, { message: 'That email is not registered' });
                     }
-                    
-                    // Match password logic (add bcrypt comparison here)
-                    done(null, user); // Placeholder - complete with bcrypt logic
+
+                    // Match password
+                    bcrypt.compare(password, user.password, (err, isMatch) => {
+                        if (err) throw err;
+                        if (isMatch) {
+                            return done(null, user);
+                        } else {
+                            return done(null, false, { message: 'Password incorrect' });
+                        }
+                    });
                 })
                 .catch(err => console.log(err));
         })
